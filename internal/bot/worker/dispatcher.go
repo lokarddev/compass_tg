ea4f221113e6abbac5990c017e4a4c62474e9b49
Oleg Babenko <lokard@MacBook-Pro-Dream.local>
2023-02-01 22:11:04 +0200

    minor edits

--- internal/bot/worker/dispatcher.go
+++ internal/bot/worker/dispatcher.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Dispatcher struct {
-	commandHandler, textHandler map[string]handler.HandlerInterface
+	commandHandler, textHandler map[string]handler.HandlersInterface
 }
 
-func (d *Dispatcher) AttachCommand(topic string, handler handler.HandlerInterface) {
+func (d *Dispatcher) AttachCommand(topic string, handler handler.HandlersInterface) {
 	d.commandHandler[topic] = handler
 }
 
-func (d *Dispatcher) AttachText(text string, handler handler.HandlerInterface) {
+func (d *Dispatcher) AttachText(text string, handler handler.HandlersInterface) {
 	d.textHandler[text] = handler
 }
 
@@ -35,7 +35,7 @@ func (d *Dispatcher) CallHandler(update *api.Update) {
 }
 
 func NewDispatcher(bot *api.BotAPI) *Dispatcher {
-	d := &Dispatcher{commandHandler: make(map[string]handler.HandlerInterface)}
+	d := &Dispatcher{commandHandler: make(map[string]handler.HandlersInterface)}
 
 	d.AttachCommand(startCommand, handler.NewStartHandler(bot))
 
